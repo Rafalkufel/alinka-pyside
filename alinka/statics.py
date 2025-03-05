98490dd36b91bc4138e703fb936e4cd6f0e60bbc
Tomasz Magulski <tomasz@magul.ski>
2025-03-05 18:36:55 +0100

    Alinka icon working when application run from PyInstaller bundle (#38)

---
+++ alinka/statics.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+
+def get_statics_resource_path(resource):
+    # see: https://pyinstaller.org/en/stable/runtime-information.html#using-sys-executable-and-sys-argv-0
+    if getattr(sys, "frozen", False):
+        dirname = sys._MEIPASS
+    else:
+        dirname = os.path.dirname(os.path.abspath(sys.argv[0]))
+
+    return os.path.join(dirname, "statics", resource)
--- alinka/widget/main_window.py
+++ alinka/widget/main_window.py
@@ -1,8 +1,7 @@
-import os
-
 from PySide2.QtGui import QIcon
 from PySide2.QtWidgets import QHBoxLayout, QWidget
 
+from alinka.statics import get_statics_resource_path
 from alinka.widget.containers.central_widget import CentralWidget
 
 
@@ -10,7 +9,7 @@ class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         icon = QIcon()
-        icon.addFile(os.path.join("statics", "alinka.svg"))
+        icon.addFile(get_statics_resource_path("alinka.svg"))
         self.setWindowTitle("Alinka")
 
         central_widget = CentralWidget(self)
