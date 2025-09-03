96354254bb694833a99bb1905bf70635be811bb3
RafalKufel <rafal.kufel@gmail.com>
2025-09-03 18:42:36 +0200

    Remove 'Zapisz' button

--- alinka/widget/containers/main_body/footer/application.py
+++ alinka/widget/containers/main_body/footer/application.py
@@ -12,10 +12,7 @@ class ApplicationFooterContainer(QFrame):
         layout.setContentsMargins(9, 9, 9, 9)
         self.print_btn = QPushButton("Drukuj dokumenty", self)
         self.print_btn.clicked.connect(self.print_documents)
-        self.save_btn = QPushButton("Zapisz", self)
-        self.save_btn.clicked.connect(self.save_document_data)
         layout.addWidget(self.print_btn)
-        layout.addWidget(self.save_btn)
 
         self.setVisible(visible)
 
@@ -35,6 +32,13 @@ class ApplicationFooterContainer(QFrame):
             return
 
     def print_documents(self) -> None:
+        content_container = self.footer_container.main_body_container.content_container
+        application_container = content_container.application_container
+        # Call validate_required_fields on the child tab
+        if not application_container.child_tab_container.validate_required_fields():
+            error_message = application_container.child_tab_container.error_message
+            content_container.main_body_container.header_container.set_error_message(error_message)
+            return
         self.validate_document_data()
 
         dialogTitle = "Wybierz katalog zapisu dokumentu"
@@ -44,14 +48,3 @@ class ApplicationFooterContainer(QFrame):
             return
 
         generate_and_save_decision(form_data=self.document_data, generate=True, destination_path=destination_path)
-
-    def save_document_data(self) -> None:
-        content_container = self.footer_container.main_body_container.content_container
-        application_container = content_container.application_container
-        # Call validate_required_fields on the child tab
-        if not application_container.child_tab_container.validate_required_fields():
-            error_message = application_container.child_tab_container.error_message
-            content_container.main_body_container.header_container.set_error_message(error_message)
-            return
-        self.validate_document_data()
-        generate_and_save_decision(form_data=self.document_data, generate=False)
